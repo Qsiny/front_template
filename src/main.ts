71da5510669b6a14947d9c2359ebf06dad51201b
Qsiny <1716439531@qq.com>
2023-04-26 20:57:18 +0800

    feat: 修复prettire的问题

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './style.css';
+import App from './App.vue';
 // vue router
 import router from '@/router/index';
 // pinia
 import store from '@/store';
 // main.ts
-import 'virtual:uno.css'
-
+import 'virtual:uno.css';
 
 const app = createApp(App);
 
 app.use(router).use(store);
+
+app.mount('#app');
